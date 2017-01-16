c1a627e0eb83da79a3bbeb46f51051d92a23b98c
H-Destiny <2026652784@qq.com>
2017-01-16 08:47:58 +0800

    遍历三级城市列表

---
+++ app/src/main/java/com/example/administrator/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.administrator.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/1/13.
+ */
+public class AQI {
+    public  AQICity city;
+
+    private class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/coolweather/gson/Suggestion.java
@@ -0,0 +1,33 @@
+package com.example.administrator.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/1/13.
+ */
+public class Suggestion {
+    @SerializedName("comf")
+    public Comfort comfort;
+
+    @SerializedName("cw")
+    public CarWash carWash;
+
+    @SerializedName("sport")
+    public Sport sport;
+
+
+    private class Comfort {
+        @SerializedName("txt")
+        public String info;
+    }
+
+    private class CarWash {
+        @SerializedName("txt")
+        public String info;
+    }
+
+    private class Sport {
+        @SerializedName("txt")
+        public String info;
+    }
+}
